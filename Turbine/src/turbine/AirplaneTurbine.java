0cd7debd39969b44cdf1993a6ba85f53bcb46042
Morten Terhart <morten.terhart@gmail.com>
2018-02-24 16:49:59 +0100

    Separated blade construction in AirplaneTurbine from constructor

--- Turbine/src/turbine/AirplaneTurbine.java
+++ Turbine/src/turbine/AirplaneTurbine.java
@@ -13,7 +13,10 @@ public class AirplaneTurbine {
 
     public AirplaneTurbine() {
         blades = new ArrayList<>();
+        buildBlades();
+    }
 
+    private void buildBlades() {
         Logger.instance.log("--- Creating the diverse shovel blades alternating Titan and Carbon ones " +
                 "starting at index 1");
         for (int index = 1; index <= 96; index += 2) {
